8f32fef49db759500dac880cf519a348bad6559f
Artanis <oficprof362@mail.ru>
2026-09-21 21:29:07 +0500

    Добавил функцию fillArray

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void fillArray(int (&arr)[10]) {
+    for (int i = 0; i < 10; i++) {
+        arr[i] = rand() % 100;
+    }
+}
 
 int main() {
+    int arr[10];
+    srand(time(0));
+    
+    fillArray(arr);
+    
+    for (int x : arr) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+    
     return 0;
 }
