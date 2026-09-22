ba5ceadcfceb6f2079c6ca77fed61e34b7f0f3ba
Artanis <oficprof362@mail.ru>
2026-09-22 16:07:18 +0500

    Добавил документацию Doxygen для printArray

--- main.cpp
+++ main.cpp
@@ -15,14 +15,21 @@ void fillArray(int (&arr)[10]) {
         arr[i] = rand() % 100;
     }
 }
+/**
+ * @brief Выводит массив на экран
+ *
+ * Перебирает все элементы массива с помощью
+ * range-based for и выводит их через пробел,
+ * затем переводит курсор на новую строку.
+ *
+ * @param arr Константная ссылка на массив из 10 элементов
+ */
 void printArray(const int (&arr)[10]) {
     for (auto x : arr) {
         std::cout << x << " ";
     }
     std::cout << std::endl;
 }
-
-
 int main() {
     int arr[10];
     srand(time(0));
