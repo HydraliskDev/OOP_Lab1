e7c5693061395359a3cfec946b9127ed2f9d83b3
Artanis <oficprof362@mail.ru>
2026-09-23 23:18:25 +0500

    Добавил функцию process и безопасное обнуление указателя

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 #include <clocale> 
+
+void process(int*& arr, int size){
+    int firstNegIdx = -1;
+    for (int i = 0; i < size; i++){
+        if (arr[i] < 0){
+            firstNegIdx = i;
+            break;
+        }
+    }
+
+if (firstNegIdx == -1){
+    std::cout << "Отрицательные элементы не найдены." << std::endl;
+    return;
+}
+
+int newSize = firstNegIdx;
+
+int* newArr = new int[newSize]{};
+
+for (int i = 0; i < newSize; i++){
+    newArr[i] = arr[i];
+}
+
+delete[] arr;
+
+arr = newArr;
+
+std::cout << "Новый размер массива: " << newSize << std::endl;
+}
+
 int main() {
     setlocale(LC_ALL, "ru_RU.UTF-8");
     int N;
@@ -19,8 +49,31 @@ int main() {
     }
     std::cout << std::endl;
 
+    int newSize = N;
+    for (int i = 0; i < N; i++){
+        if (arr[i] < 0){
+            newSize = i;
+            break;
+        }
+    }
+
+    process(arr, N);
+
+    std::cout << "Результат: ";
+    for (int i = 0; i < newSize; i++){
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
     delete[] arr;
     arr = nullptr;
+
+    if (arr == nullptr){
+        std::cout << "Указатель обнулен - безопасно" << std::endl;
+    }
+    else{
+        std::cout << *arr << std::endl;
+    }
 
     return 0;
 
